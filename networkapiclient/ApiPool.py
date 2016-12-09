8c8e755953a51a478a09f8be4b37d56f2125dcc5
juanaugusto <juan.paula@corp.globo.com>
2016-12-09 13:30:16 -0200

    Creating Pool new v3 callings

--- networkapiclient/ApiPool.py
+++ networkapiclient/ApiPool.py
@@ -15,6 +15,7 @@
 # limitations under the License.
 
 from networkapiclient.ApiGenericClient import ApiGenericClient
+from utils import build_uri_with_ids
 
 
 class ApiPool(ApiGenericClient):
@@ -42,7 +43,7 @@ class ApiPool(ApiGenericClient):
 
         uri = "api/v3/pool/environment-vip/%s/" % environment_vip_id
 
-        return self.get(uri)
+        return super(ApiPool, self).get(uri)
 
     def pool_by_id(self, pool_id):
         """
@@ -53,7 +54,7 @@ class ApiPool(ApiGenericClient):
 
         uri = "api/v3/pool/%s/" % pool_id
 
-        return self.get(uri)
+        return super(ApiPool, self).get(uri)
 
     def get_pool_details(self, pool_id):
         """
@@ -64,4 +65,72 @@ class ApiPool(ApiGenericClient):
 
         uri = "api/v3/pool/details/%s/" % pool_id
 
-        return self.get(uri)
+        return super(ApiPool, self).get(uri)
+    
+    def search(self, **kwargs):
+        """
+        Method to search pool's based on extends search.
+
+        :param search: Dict containing QuerySets to find pool's.
+        :param include: Array containing fields to include on response.
+        :param exclude: Array containing fields to exclude on response.
+        :param fields:  Array containing fields to override default fields.
+        :param kind: Determine if result will be detailed ('detail') or basic ('basic').
+        :return: Dict containing pool's
+        """
+
+        return super(ApiPool, self).get(self.prepare_url('api/v3/pool/',
+                                                                kwargs))
+
+    def get(self, ids, **kwargs):
+        """
+        Method to get pool's by their ids
+
+        :param ids: List containing identifiers of pool's
+        :param include: Array containing fields to include on response.
+        :param exclude: Array containing fields to exclude on response.
+        :param fields: Array containing fields to override default fields.
+        :param kind: Determine if result will be detailed ('detail') or basic ('basic').
+        :return: Dict containing pool's
+        """
+        url = build_uri_with_ids('api/v3/pool/%s/', ids)
+
+        return super(ApiPool, self).get(self.prepare_url(url, kwargs))
+
+    def delete(self, ids):
+        """
+        Method to delete pool's by their ids
+
+        :param ids: Identifiers of pool's
+        :return: None
+        """
+        url = build_uri_with_ids('api/v3/pool/%s/', ids)
+
+        return super(ApiPool, self).delete(url)
+
+    def update(self, pools):
+        """
+        Method to update pool's
+
+        :param pools: List containing pool's desired to updated
+        :return: None
+        """
+
+        data = {'server_pools': pools}
+        pools_ids = [str(pool.get('id'))
+                            for pool in pools]
+
+        return super(ApiPool, self).put('api/v3/pool/%s/' %
+                                               ';'.join(pools_ids), data)
+
+    def create(self, pools):
+        """
+        Method to create pool's
+
+        :param pools: List containing pool's desired to be created on database
+        :return: None
+        """
+
+        data = {'server_pools': pools}
+        return super(ApiPool, self).post('api/v3/pool/', data)
+
