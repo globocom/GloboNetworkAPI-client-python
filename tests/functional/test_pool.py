da1db8abbb0e1712c38b13c9541baba24a376ea4
juanaugusto <juan.paula@corp.globo.com>
2017-01-11 15:33:51 -0200

    Adding more pool functional tests

--- tests/functional/test_pool.py
+++ tests/functional/test_pool.py
@@ -64,37 +64,15 @@ class TestApiPool(TestCase):
         assert_equal(pool['server_pools'][0]['id'], pool_id)
         self.api_pool.delete([pool_id])
 
-    def test_create_pool_with_two_reals_and_https_protocol(self):
-        """ Tries to create a pool with one real and https protocol in healthcheck """
+    def test_create_pool_with_one_real_and_https_protocol(self):
+        """ Tries to create a pool with one real and HTTPS protocol in healthcheck """
 
-        id_env_of_eqpt = self.create_environment_of_equipment()
-        id_env_of_pool = self.create_environment_of_pool()
-
-        id_env_vip = self.create_environment_vip(id_env_of_eqpt)
-
-        id_vlan_of_env_eqpt = self.create_vlan(id_env_of_eqpt)
-        id_vlan_of_env_pool = self.create_vlan(id_env_of_pool)
-
-        id_netipv4_of_vlan_env_eqpt = self.create_netipv4(id_vlan_of_env_eqpt, None)
-        id_netipv6_of_vlan_env_eqpt = self.create_netipv6(id_vlan_of_env_eqpt, None)
-
-        id_netipv4_of_vlan_env_pool = self.create_netipv4(id_vlan_of_env_pool, id_env_vip)
-        id_netipv6_of_vlan_env_pool = self.create_netipv6(id_vlan_of_env_pool, id_env_vip)
-
-        # creating three reals (servers) equipments
-
-        id_reals = [self.create_real_equipment(i, id_env_of_eqpt) for i in range(1)]
-
-        id_load_balancer = self.create_load_balancer_equipment(id_env_of_pool)
-
-        ipsv4_of_reals = [self.create_ipv4(id_netipv4_of_vlan_env_eqpt) for id_real in id_reals]
-
-        ipsv6_of_reals = [self.create_ipv6(id_netipv6_of_vlan_env_eqpt) for id_real in id_reals]
+        initial_data = self.create_initial_data(qt_reals=range(1))
 
         pool_data = [{
             'identifier': 'Pool-Test-Network-API-With-Real-And-HTTPS-Prot',
             'default_port': 443,
-            'environment': id_env_of_pool,
+            'environment': initial_data['id_env_of_pool'],
             'servicedownaction': {
                 'id': 5,
                 'name': 'none'
@@ -111,8 +89,8 @@ class TestApiPool(TestCase):
             'server_pool_members': [
                 {
                     'ip': {
-                        'id': ipsv4_of_reals[0]['id'],
-                        'ip_formated': ipsv4_of_reals[0]['ip_formated']
+                        'id': initial_data['ipsv4_of_reals'][0]['id'],
+                        'ip_formated': initial_data['ipsv4_of_reals'][0]['ip_formated']
                     },
                     'ipv6': None,
                     'priority': 0,
@@ -122,7 +100,7 @@ class TestApiPool(TestCase):
                     'member_status': 7,
                     'last_status_update_formated': None,
                     'equipment': {
-                        'id': id_reals[0]
+                        'id': initial_data['id_reals'][0]
                     }
                 }
             ],
@@ -135,82 +113,161 @@ class TestApiPool(TestCase):
         assert_equal(pool['server_pools'][0]['id'], pool_id)
         self.api_pool.delete([pool_id])
 
-        # deleting all created
+        self.delete_initial_data(initial_data)
 
-        for ipv4 in ipsv4_of_reals:
-            self.api_ipv4.delete([ipv4['id']])
-
-        for ipv6 in ipsv6_of_reals:
-            self.api_ipv6.delete([ipv6['id']])
-
-        self.api_network_ipv4.delete([id_netipv4_of_vlan_env_eqpt, id_netipv4_of_vlan_env_pool])
-        self.api_network_ipv6.delete([id_netipv6_of_vlan_env_eqpt, id_netipv6_of_vlan_env_pool])
-
-        self.api_vlan.delete([id_vlan_of_env_eqpt, id_vlan_of_env_pool])
-
-        self.api_equipment.delete(id_reals)
-        self.api_equipment.delete([id_load_balancer])
-
-        self.api_environment_vip.delete([id_env_vip])
-
-        self.api_environment.delete([id_env_of_eqpt, id_env_of_pool])
 
     def test_create_pool_with_one_real_and_tcp_protocol(self):
-        """ Tries to create a pool with one real and tcp protocol """
+        """ Tries to create a pool with one real and TCP protocol in healthcheck """
 
-        # pool_data = [{
-        #     'identifier': 'Pool-Test-Network-API-With-Real-And-TCP-Prot',
-        #     'default_port': 20,
-        #     'environment': 1,
-        #     'servicedownaction': {
-        #         'id': 5,
-        #         'name': 'none'
-        #     },
-        #     'lb_method': 'least-conn',
-        #     'healthcheck': {
-        #         'identifier': 'Test-Network-API-Ident',
-        #         'healthcheck_type': 'TCP',
-        #         'healthcheck_request': '',
-        #         'healthcheck_expect': '',
-        #         'destination': '*:*'
-        #     },
-        #     'default_limit': 0,
-        #     'server_pool_members': [
-        #         {
-        #             'ip': {
-        #                 'id': 7,
-        #                 'ip_formated': '10.237.128.1'
-        #             },
-        #             'ipv6': None,
-        #             'priority': 0,
-        #             'weight': 0,
-        #             'limit': 0,
-        #             'port_real': 20,
-        #             'member_status': 7,
-        #             'last_status_update_formated': None,
-        #             'equipment': {
-        #                 'id': 14
-        #             }
-        #         }
-        #     ],
-        #     'pool_created': False
-        # }]
-        #
-        # pool_id = self.api_pool.create(pool_data)[0]['id']
-        # pool = self.api_pool.get([pool_id])
-        #
-        # assert_equal(pool['server_pools'][0]['id'], pool_id)
-        # self.api_pool.delete([pool_id])
+        initial_data = self.create_initial_data(qt_reals=range(1))
+
+        pool_data = [{
+            'identifier': 'Pool-Test-Network-API-With-Real-And-HTTPS-Prot',
+            'default_port': 443,
+            'environment': initial_data['id_env_of_pool'],
+            'servicedownaction': {
+                'id': 5,
+                'name': 'none'
+            },
+            'lb_method': 'least-conn',
+            'healthcheck': {
+                'identifier': 'Test-Network-API-Ident',
+                'healthcheck_type': 'TCP',
+                'healthcheck_request': '',
+                'healthcheck_expect': '',
+                'destination': '*:*'
+            },
+            'default_limit': 0,
+            'server_pool_members': [
+                {
+                    'ip': {
+                        'id': initial_data['ipsv4_of_reals'][0]['id'],
+                        'ip_formated': initial_data['ipsv4_of_reals'][0]['ip_formated']
+                    },
+                    'ipv6': None,
+                    'priority': 0,
+                    'weight': 0,
+                    'limit': 0,
+                    'port_real': 442,
+                    'member_status': 7,
+                    'last_status_update_formated': None,
+                    'equipment': {
+                        'id': initial_data['id_reals'][0]
+                    }
+                }
+            ],
+            'pool_created': False
+        }]
+
+        pool_id = self.api_pool.create(pool_data)[0]['id']
+        pool = self.api_pool.get([pool_id])
+
+        assert_equal(pool['server_pools'][0]['id'], pool_id)
+        self.api_pool.delete([pool_id])
+
+        self.delete_initial_data(initial_data)
 
     def test_create_pool_with_one_real_and_udp_protocol(self):
-        """ Tries to create a pool with one real and udp protocol """
+        """ Tries to create a pool with one real and UDP protocol in healthcheck """
 
-        pass
+        initial_data = self.create_initial_data(qt_reals=range(1))
+
+        pool_data = [{
+            'identifier': 'Pool-Test-Network-API-With-Real-And-HTTPS-Prot',
+            'default_port': 443,
+            'environment': initial_data['id_env_of_pool'],
+            'servicedownaction': {
+                'id': 5,
+                'name': 'none'
+            },
+            'lb_method': 'least-conn',
+            'healthcheck': {
+                'identifier': 'Test-Network-API-Ident',
+                'healthcheck_type': 'UDP',
+                'healthcheck_request': '',
+                'healthcheck_expect': '',
+                'destination': '*:*'
+            },
+            'default_limit': 0,
+            'server_pool_members': [
+                {
+                    'ip': {
+                        'id': initial_data['ipsv4_of_reals'][0]['id'],
+                        'ip_formated': initial_data['ipsv4_of_reals'][0]['ip_formated']
+                    },
+                    'ipv6': None,
+                    'priority': 0,
+                    'weight': 0,
+                    'limit': 0,
+                    'port_real': 442,
+                    'member_status': 7,
+                    'last_status_update_formated': None,
+                    'equipment': {
+                        'id': initial_data['id_reals'][0]
+                    }
+                }
+            ],
+            'pool_created': False
+        }]
+
+        pool_id = self.api_pool.create(pool_data)[0]['id']
+        pool = self.api_pool.get([pool_id])
+
+        assert_equal(pool['server_pools'][0]['id'], pool_id)
+        self.api_pool.delete([pool_id])
+
+        self.delete_initial_data(initial_data)
 
     def test_create_pool_with_one_real_and_http_protocol(self):
-        """ Tries to create a pool with one real and http protocol """
+        """ Tries to create a pool with one real and HTTP protocol in healthcheck"""
 
-        pass
+        initial_data = self.create_initial_data(qt_reals=range(1))
+
+        pool_data = [{
+            'identifier': 'Pool-Test-Network-API-With-Real-And-HTTPS-Prot',
+            'default_port': 443,
+            'environment': initial_data['id_env_of_pool'],
+            'servicedownaction': {
+                'id': 5,
+                'name': 'none'
+            },
+            'lb_method': 'least-conn',
+            'healthcheck': {
+                'identifier': 'Test-Network-API-Ident',
+                'healthcheck_type': 'HTTP',
+                'healthcheck_request': '',
+                'healthcheck_expect': '',
+                'destination': '*:*'
+            },
+            'default_limit': 0,
+            'server_pool_members': [
+                {
+                    'ip': {
+                        'id': initial_data['ipsv4_of_reals'][0]['id'],
+                        'ip_formated': initial_data['ipsv4_of_reals'][0]['ip_formated']
+                    },
+                    'ipv6': None,
+                    'priority': 0,
+                    'weight': 0,
+                    'limit': 0,
+                    'port_real': 442,
+                    'member_status': 7,
+                    'last_status_update_formated': None,
+                    'equipment': {
+                        'id': initial_data['id_reals'][0]
+                    }
+                }
+            ],
+            'pool_created': False
+        }]
+
+        pool_id = self.api_pool.create(pool_data)[0]['id']
+        pool = self.api_pool.get([pool_id])
+
+        assert_equal(pool['server_pools'][0]['id'], pool_id)
+        self.api_pool.delete([pool_id])
+
+        self.delete_initial_data(initial_data)
 
     def test_create_pool_with_three_reals_and_weight_balancing(self):
         """ Tries to create a pool with three reals and weight balancing """
@@ -398,3 +455,72 @@ class TestApiPool(TestCase):
 
         id_ip = self.api_ipv6.create(ip_data)[0]['id']
         return self.api_ipv6.get([id_ip], fields=['id', 'ip_formated'])['ips'][0]
+
+    def create_initial_data(self, qt_reals):
+
+        id_env_of_eqpt = self.create_environment_of_equipment()
+        id_env_of_pool = self.create_environment_of_pool()
+
+        id_env_vip = self.create_environment_vip(id_env_of_eqpt)
+
+        id_vlan_of_env_eqpt = self.create_vlan(id_env_of_eqpt)
+        id_vlan_of_env_pool = self.create_vlan(id_env_of_pool)
+
+        id_netipv4_of_vlan_env_eqpt = self.create_netipv4(id_vlan_of_env_eqpt, None)
+        id_netipv6_of_vlan_env_eqpt = self.create_netipv6(id_vlan_of_env_eqpt, None)
+
+        id_netipv4_of_vlan_env_pool = self.create_netipv4(id_vlan_of_env_pool, id_env_vip)
+        id_netipv6_of_vlan_env_pool = self.create_netipv6(id_vlan_of_env_pool, id_env_vip)
+
+        # creating three reals (servers) equipments
+
+        id_reals = [self.create_real_equipment(i, id_env_of_eqpt) for i in qt_reals]
+
+        id_load_balancer = self.create_load_balancer_equipment(id_env_of_pool)
+
+        ipsv4_of_reals = [self.create_ipv4(id_netipv4_of_vlan_env_eqpt) for id_real in id_reals]
+
+        ipsv6_of_reals = [self.create_ipv6(id_netipv6_of_vlan_env_eqpt) for id_real in id_reals]
+
+        return {
+            'id_env_of_eqpt' : id_env_of_eqpt,
+            'id_env_of_pool' : id_env_of_pool,
+            'id_env_vip': id_env_vip,
+            'id_vlan_of_env_eqpt': id_vlan_of_env_eqpt,
+            'id_vlan_of_env_pool': id_vlan_of_env_pool,
+            'id_netipv4_of_vlan_env_eqpt': id_netipv4_of_vlan_env_eqpt,
+            'id_netipv6_of_vlan_env_eqpt': id_netipv6_of_vlan_env_eqpt,
+            'id_netipv4_of_vlan_env_pool': id_netipv4_of_vlan_env_pool,
+            'id_netipv6_of_vlan_env_pool': id_netipv6_of_vlan_env_pool,
+            'id_reals': id_reals,
+            'id_load_balancer': id_load_balancer,
+            'ipsv4_of_reals': ipsv4_of_reals,
+            'ipsv6_of_reals': ipsv6_of_reals
+        }
+
+
+    def delete_initial_data(self, initial_data):
+
+        for ipv4 in initial_data['ipsv4_of_reals']:
+            self.api_ipv4.delete([ipv4['id']])
+
+        for ipv6 in initial_data['ipsv6_of_reals']:
+            self.api_ipv6.delete([ipv6['id']])
+
+        self.api_network_ipv4.delete([initial_data['id_netipv4_of_vlan_env_eqpt']])
+        self.api_network_ipv4.delete([initial_data['id_netipv4_of_vlan_env_pool']])
+        self.api_network_ipv6.delete([initial_data['id_netipv6_of_vlan_env_eqpt']])
+        self.api_network_ipv6.delete([initial_data['id_netipv6_of_vlan_env_pool']])
+
+        self.api_vlan.delete([initial_data['id_vlan_of_env_eqpt']])
+        self.api_vlan.delete([initial_data['id_vlan_of_env_pool']])
+
+        self.api_equipment.delete(initial_data['id_reals'])
+        self.api_equipment.delete([initial_data['id_load_balancer']])
+
+        self.api_environment_vip.delete([initial_data['id_env_vip']])
+
+        self.api_environment.delete([initial_data['id_env_of_eqpt']])
+        self.api_environment.delete([initial_data['id_env_of_pool']])
+
+
