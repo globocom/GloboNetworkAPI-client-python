4f89b7a26e40c497a469684de6481bf8b65042dd
Renan Lopes <renan.lopes88@gmail.com>
2024-08-27 14:33:43 -0300

    fixed unit tests for networkipv4

--- tests/functional/test_network_ipv4.py
+++ tests/functional/test_network_ipv4.py
@@ -2,17 +2,10 @@
 import os
 from unittest import TestCase
 
-from nose.tools import assert_equal
-from nose.tools import assert_greater
-from nose.tools import assert_in
-from nose.tools import assert_is_instance
-from nose.tools import assert_raises
-from nose.tools import assert_true
-
 from networkapiclient.ClientFactory import ClientFactory
 from networkapiclient.exception import NetworkAPIClientError
 
-NETWORKAPI_URL = os.getenv('NETWORKAPI_URL', 'http://10.0.0.2:8000/')
+NETWORKAPI_URL = os.getenv('NETWORKAPI_URL', 'http://localhost:8000/')
 NETWORKAPI_USER = os.getenv('NETWORKAPI_USER', 'networkapi')
 NETWORKAPI_PWD = os.getenv('NETWORKAPI_PWD', 'networkapi')
 
@@ -31,8 +24,8 @@ class TestNetworkIPv4(TestCase):
 
         networks = self.api_net_ipv4.list()
 
-        assert_is_instance(networks, list)
-        assert_greater(len(networks), 1)
+        self.assertIsInstance(networks, list)
+        self.assertGreater(len(networks), 1)
 
     def test_create_new_ipv4_network_dinamically_by_prefix(self):
         """ Create a new IPv4 network """
@@ -45,8 +38,8 @@ class TestNetworkIPv4(TestCase):
 
         network_id = self.api_net_ipv4.create([data])[0]['id']
         network = self.api_net_ipv4.get([network_id])['networks'][0]
-        assert_equal(network['prefix'], 28)
-        assert_equal(network['broadcast'], '10.0.1.15')
+        self.assertEqual(network['prefix'], 28)
+        self.assertEqual(network['broadcast'], '10.0.1.15')
 
         self.api_net_ipv4.delete([network_id])
 
@@ -67,9 +60,9 @@ class TestNetworkIPv4(TestCase):
         network_id = self.api_net_ipv4.create([data])[0]['id']
         network = self.api_net_ipv4.get([network_id])['networks'][0]
 
-        assert_equal(network['prefix'], 30)
-        assert_equal(network['broadcast'], '10.0.1.3')
-        assert_equal(network['mask_oct4'], 252)
+        self.assertEqual(network['prefix'], 30)
+        self.assertEqual(network['broadcast'], '10.0.1.3')
+        self.assertEqual(network['mask_oct4'], 252)
 
         self.api_net_ipv4.delete([network_id])
 
@@ -84,9 +77,9 @@ class TestNetworkIPv4(TestCase):
         network_id = self.api_net_ipv4.create([data])[0]['id']
         network = self.api_net_ipv4.get([network_id])['networks'][0]
 
-        assert_equal(network['vlan'], data['vlan'])
-        assert_equal(network['network_type'], data['network_type'])
-        assert_equal(network['id'], network_id)
+        self.assertEqual(network['vlan'], data['vlan'])
+        self.assertEqual(network['network_type'], data['network_type'])
+        self.assertEqual(network['id'], network_id)
 
         self.api_net_ipv4.delete([network_id])
 
@@ -101,13 +94,13 @@ class TestNetworkIPv4(TestCase):
         network_id = self.api_net_ipv4.create([data])[0]['id']
         response = self.api_net_ipv4.delete([network_id])
 
-        assert_is_instance(response, list)
-        assert_equal(len(response), 0)
+        self.assertIsInstance(response, list)
+        self.assertEqual(len(response), 0)
 
     def test_delete_a_non_existent_ipv4_network(self):
         """ Tries to delete a non existent ipv4 network """
 
-        with assert_raises(NetworkAPIClientError):
+        with self.assertRaises(NetworkAPIClientError):
             response = self.api_net_ipv4.delete([5555])
 
     def test_delete_an_active_network(self):
@@ -115,7 +108,7 @@ class TestNetworkIPv4(TestCase):
 
         active_network_id = 7
 
-        with assert_raises(NetworkAPIClientError):
+        with self.assertRaises(NetworkAPIClientError):
             self.api_net_ipv4.delete([active_network_id])
 
     def test_update_network(self):
@@ -133,7 +126,7 @@ class TestNetworkIPv4(TestCase):
         self.api_net_ipv4.update([data])
 
         network = self.api_net_ipv4.get([network_id])['networks'][0]
-        assert_equal(network['network_type'], data['network_type'])
+        self.assertEqual(network['network_type'], data['network_type'])
 
         self.api_net_ipv4.delete([network_id])
 
@@ -149,7 +142,7 @@ class TestNetworkIPv4(TestCase):
         self.api_net_ipv4.update([data])
 
         network = self.api_net_ipv4.get([data['id']])['networks'][0]
-        assert_true(network['active'])
+        self.assertTrue(network['active'])
 
     def test_create_network_on_an_environment_that_have_a_router(self):
         """ Creates a network on an environment that have a router """
@@ -172,7 +165,7 @@ class TestNetworkIPv4(TestCase):
             for equipment in ip['equipments']:
                 equipments_ids.append(equipment['id'])
 
-        assert_in(expected_equipament_id, equipments_ids)
+        self.assertIn(expected_equipament_id, equipments_ids)
         self.api_net_ipv4.delete([network_id])
 
     def test_create_network_on_an_environment_that_have_two_routers(self):
@@ -196,7 +189,7 @@ class TestNetworkIPv4(TestCase):
             for equipment in ip['equipments']:
                 equipments_ids.append(equipment['id'])
 
-        assert_in(expected_equipaments_id[0], equipments_ids)
-        assert_in(expected_equipaments_id[1], equipments_ids)
+        self.assertIn(expected_equipaments_id[0], equipments_ids)
+        self.assertIn(expected_equipaments_id[1], equipments_ids)
 
         self.api_net_ipv4.delete([network_id])
