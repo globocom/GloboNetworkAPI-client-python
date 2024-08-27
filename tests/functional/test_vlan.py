7d8844bd8a69e3e08bae1cb5ca02a73c94ed1df8
Renan Lopes <renan.lopes88@gmail.com>
2024-08-27 14:36:24 -0300

    fixed unit tests for vlan

--- tests/functional/test_vlan.py
+++ tests/functional/test_vlan.py
@@ -2,15 +2,11 @@
 import os
 from unittest import TestCase
 
-from nose.tools import assert_equal
-from nose.tools import assert_in
-from nose.tools import assert_raises
-
 from networkapiclient.ClientFactory import ClientFactory
 from networkapiclient.exception import NetworkAPIClientError
 
 
-NETWORKAPI_URL = os.getenv('NETWORKAPI_URL', 'http://10.0.0.2:8000/')
+NETWORKAPI_URL = os.getenv('NETWORKAPI_URL', 'http://localhost:8000/')
 NETWORKAPI_USER = os.getenv('NETWORKAPI_USER', 'networkapi')
 NETWORKAPI_PWD = os.getenv('NETWORKAPI_PWD', 'networkapi')
 
@@ -34,8 +30,8 @@ class TestApiVlan(TestCase):
 
         vlans = self.api_vlan.search(search=search_data)
 
-        assert_equal(vlans['total'], 1)
-        assert_equal(vlans['vlans'][0]['name'], vlan_name)
+        self.assertEqual(vlans['total'], 1)
+        self.assertEqual(vlans['vlans'][0]['name'], vlan_name)
 
     def test_get_vlans_by_id(self):
         """ Gets vlans by ids """
@@ -43,9 +39,9 @@ class TestApiVlan(TestCase):
 
         vlans = self.api_vlan.get(vlans_ids)
 
-        assert_equal(len(vlans['vlans']), 3)
+        self.assertEqual(len(vlans['vlans']), 3)
         for vlan in vlans['vlans']:
-            assert_in(vlan['id'], vlans_ids)
+            self.assertIn(vlan['id'], vlans_ids)
 
     def test_get_only_one_vlan_by_id(self):
         """ Gets only one vlan by its id """
@@ -53,14 +49,14 @@ class TestApiVlan(TestCase):
         vlan_id = 7
         vlan = self.api_vlan.get([vlan_id])
 
-        assert_equal(len(vlan['vlans']), 1)
-        assert_equal(vlan['vlans'][0]['id'], vlan_id)
+        self.assertEqual(len(vlan['vlans']), 1)
+        self.assertEqual(vlan['vlans'][0]['id'], vlan_id)
 
     def test_get_a_non_existing_vlan(self):
         """ Attenpts to get a non existing vlan """
 
         vlan_id = 15
-        with assert_raises(NetworkAPIClientError):
+        with self.assertRaises(NetworkAPIClientError):
             vlan = self.api_vlan.get([vlan_id])
 
     def test_insert_new_vlan(self):
@@ -73,8 +69,8 @@ class TestApiVlan(TestCase):
         vlan_id = self.api_vlan.create([vlan_data])[0]['id']
         vlan = self.api_vlan.get([vlan_id])
 
-        assert_equal(len(vlan['vlans']), 1)
-        assert_equal(vlan['vlans'][0]['id'], vlan_id)
+        self.assertEqual(len(vlan['vlans']), 1)
+        self.assertEqual(vlan['vlans'][0]['id'], vlan_id)
 
         self.api_vlan.delete([vlan_id])
 
@@ -89,7 +85,7 @@ class TestApiVlan(TestCase):
 
         vlan_id = self.api_vlan.create([vlan_data])[0]['id']
 
-        with assert_raises(NetworkAPIClientError):
+        with self.assertRaises(NetworkAPIClientError):
             self.api_vlan.create([vlan_data])
 
         self.api_vlan.delete([vlan_id])
@@ -111,9 +107,9 @@ class TestApiVlan(TestCase):
         vlans_ids = [vlan['id'] for vlan in self.api_vlan.create(vlans_data)]
         vlans = self.api_vlan.get(vlans_ids)
 
-        assert_equal(len(vlans['vlans']), 3)
+        self.assertEqual(len(vlans['vlans']), 3)
         for vlan in vlans['vlans']:
-            assert_in(vlan['id'], vlans_ids)
+            self.assertIn(vlan['id'], vlans_ids)
 
         self.api_vlan.delete(vlans_ids)
 
@@ -126,7 +122,7 @@ class TestApiVlan(TestCase):
             'fake_data': True,
         }
 
-        with assert_raises(NetworkAPIClientError):
+        with self.assertRaises(NetworkAPIClientError):
             self.api_vlan.create([vlan_data])
 
     def test_delete_a_single_vlan(self):
@@ -140,7 +136,7 @@ class TestApiVlan(TestCase):
         vlan_id = self.api_vlan.create([vlan_data])[0]['id']
         self.api_vlan.delete([vlan_id])
 
-        with assert_raises(NetworkAPIClientError):
+        with self.assertRaises(NetworkAPIClientError):
             vlan = self.api_vlan.get([vlan_id])
 
     def test_delete_a_list_of_vlans(self):
@@ -160,13 +156,13 @@ class TestApiVlan(TestCase):
         vlans_ids = [vlan['id'] for vlan in self.api_vlan.create(vlans_data)]
         self.api_vlan.delete(vlans_ids)
 
-        with assert_raises(NetworkAPIClientError):
+        with self.assertRaises(NetworkAPIClientError):
             self.api_vlan.get(vlans_ids)
 
     def test_delete_a_non_existent_vlan(self):
         """ Tries to delete a non existent vlan """
 
-        with assert_raises(NetworkAPIClientError):
+        with self.assertRaises(NetworkAPIClientError):
             self.api_vlan.delete([44])
 
     def test_update_a_vlan(self):
@@ -184,7 +180,7 @@ class TestApiVlan(TestCase):
         self.api_vlan.update([vlan])
         vlan = self.api_vlan.get([vlan_id])['vlans'][0]
 
-        assert_equal(vlan['name'], new_name)
+        self.assertEqual(vlan['name'], new_name)
 
         self.api_vlan.delete([vlan_id])
 
@@ -194,7 +190,7 @@ class TestApiVlan(TestCase):
         vlan = self.api_vlan.get([1])['vlans'][0]
         vlan['id'] = 55
 
-        with assert_raises(NetworkAPIClientError):
+        with self.assertRaises(NetworkAPIClientError):
             self.api_vlan.update([vlan])
 
     def test_create_vlan_with_unauthorized_number_for_environment(self):
@@ -204,5 +200,5 @@ class TestApiVlan(TestCase):
             'environment': 3,
         }
 
-        with assert_raises(NetworkAPIClientError):
+        with self.assertRaises(NetworkAPIClientError):
             self.api_vlan.create([vlan_data])
